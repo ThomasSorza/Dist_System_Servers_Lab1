066af64c364ed4ca2a2af54e74d498100bf2af12
EstebanBarrero <barrerodavid10@hotmail.com>
2023-08-20 01:11:12 -0500

    waiting queue

--- Load_Balancer/model/test.py
+++ Load_Balancer/model/test.py
@@ -2,41 +2,49 @@ import socket
 import threading
 from main_server import Server
 
-MAX_CLIENTS = 5
+MAX_CLIENTS = 1
 PORT = 446
 
 def handle_client(client_socket):
     try:
         while True:
-            data = client_socket.recv(1024)  # Receive data from the client
+            data = client_socket.recv(1024)  # Recibe datos del cliente
 
             if not data:
-                break  # If no data received, the client has disconnected
+                break  # Si no se recibe ningún dato, el cliente se ha desconectado
 
-            message = data.decode('utf-8')  # Convert the received bytes to a string
+            message = data.decode('utf-8')  # Convierte los bytes recibidos a una cadena
             response = f"Received: {message}"
-            client_socket.send(response.encode('utf-8'))  # Send a response back to the client
+            client_socket.send(response.encode('utf-8'))  # Envía una respuesta de vuelta al cliente
 
     except Exception as e:
         print(f"Error while handling client: {e}")
 
     finally:
-        client_socket.close()  # Close the client socket when done
+        client_socket.close()  # Cierra el socket del cliente cuando termina
 
 def main():
-    server = Server(446, 8)
-    server.bind()
-    server.startListening()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', PORT))
+    server_socket.listen(5)
 
-    print(f"Server listening on port {server}...")
+    print(f"Server listening on port {PORT}...")
+
+    # Lista para mantener las conexiones activas
+    active_clients = []
 
     while True:
         try:
-            client_socket, client_address = server.accept()
+            client_socket, client_address = server_socket.accept()
             print(f"Connection from: {client_address}")
 
-            # Start a new thread to handle the client connection
-            threading.Thread(target=handle_client, args=(client_socket,)).start()
+            # Verifica si se ha alcanzado el límite máximo de clientes
+            if len(active_clients) < MAX_CLIENTS:
+                active_clients.append(client_socket)
+                threading.Thread(target=handle_client, args=(client_socket,)).start()
+            else:
+                print("Maximum number of customers reached. Putting the customer on hold.")
+                
 
         except KeyboardInterrupt:
             print("Server shutting down.")
