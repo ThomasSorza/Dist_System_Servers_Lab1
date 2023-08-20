1db6f629f84eca177310c14c8e0823b3e1e8bbc4
Thomas <sthx09567541@gmail.com>
2023-08-19 21:41:03 -0500

    completed 1st req

--- Load_Balancer/model/client.py
+++ Load_Balancer/model/client.py
@@ -1,12 +1,41 @@
 import socket
+import time
 
-HOST = input("Enter the add to connect: ")
-PORT = 446 #port to connect
+def main():
+    host = '127.0.0.1'  # Server's IP address or hostname
+    port = 446         # Server's port
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create a socket object as INET ipv4 and TCP socket
-socket.connect((HOST, PORT)) #connect to the server
+    while True:
+        try:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((host, port))
+            print("Connected to the server.")
 
-socket.send(bytes(input("Enter Message to the server"), "utf-8")) #send a message to the server
-print(socket.recv(1024).decode('utf-8')) #receive the message from the server
+            try:
+                while True:
+                    message = input("Enter a message (or 'exit' to quit): ")
+                    
+                    if message.lower() == 'exit':
+                        break  # Exit the loop if the user types 'exit'
+                    
+                    client_socket.send(message.encode('utf-8'))
+                    response = client_socket.recv(1024).decode('utf-8')
+                    print("Server:", response)
 
-#socket basico (crear clase cliente que herede de socket.socket)
+            except KeyboardInterrupt:
+                print("\nKeyboard Interrupt. Closing client...")
+                break
+
+            finally:
+                client_socket.close()
+
+        except (ConnectionError, ConnectionAbortedError, ConnectionResetError):
+            print("Connection to the server lost. Reconnecting...")
+            try:
+                time.sleep(2)  # Wait for a short period before attempting to reconnect
+            except KeyboardInterrupt:
+                    print("\nKeyboard Interrupt. Closing client...")
+                    break
+
+if __name__ == "__main__":
+    main()
---
+++ Load_Balancer/model/test.py
@@ -0,0 +1,46 @@
+import socket
+import threading
+from main_server import Server
+
+MAX_CLIENTS = 5
+PORT = 446
+
+def handle_client(client_socket):
+    try:
+        while True:
+            data = client_socket.recv(1024)  # Receive data from the client
+
+            if not data:
+                break  # If no data received, the client has disconnected
+
+            message = data.decode('utf-8')  # Convert the received bytes to a string
+            response = f"Received: {message}"
+            client_socket.send(response.encode('utf-8'))  # Send a response back to the client
+
+    except Exception as e:
+        print(f"Error while handling client: {e}")
+
+    finally:
+        client_socket.close()  # Close the client socket when done
+
+def main():
+    server = Server(446, 8)
+    server.bind()
+    server.startListening()
+
+    print(f"Server listening on port {server}...")
+
+    while True:
+        try:
+            client_socket, client_address = server.accept()
+            print(f"Connection from: {client_address}")
+
+            # Start a new thread to handle the client connection
+            threading.Thread(target=handle_client, args=(client_socket,)).start()
+
+        except KeyboardInterrupt:
+            print("Server shutting down.")
+            break
+
+if __name__ == "__main__":
+    main()
