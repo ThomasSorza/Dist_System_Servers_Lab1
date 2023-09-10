bcdcfd92fcf593f5184f60d7ac1930adbea43888
EstebanBarrero <barrerodavid10@hotmail.com>
2023-09-10 14:10:03 -0500

    Commit 001: Creacion de tablas

--- Lab2_Crud/Lab2_Crud/settings.py
+++ Lab2_Crud/Lab2_Crud/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Edit by Esteban
+LANGUAGE_CODE = 'es'  # Código de idioma español
+DATE_FORMAT = 'd/m/Y'  # Formato de fecha "día/mes/año"
--- Lab2_Crud/crud/admin.py
+++ Lab2_Crud/crud/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Users
+from .models import Roles
 
 # Register your models here.
+
+admin.site.register(Users)
+admin.site.register(Roles)
