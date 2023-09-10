1ee8916850f532362253b2415db9620d0a0bf059
Thomas <sthx09567541@gmail.com>
2023-09-10 04:16:06 -0500

    added Lab2_Crud Django project

---
+++ Lab2_Crud/Lab2_Crud/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Lab2_Crud project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Lab2_Crud.settings')
+
+application = get_asgi_application()
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Dist_System_Servers_Lab1
-Distributed System Laboratory number 1: Servers, Client-Server Model 
+Distributed System Laboratory number 1: Servers, Client-Server Model
+Laboratory number 2: Crud using Django ORM and MySQL
