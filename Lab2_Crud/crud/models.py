c0145d6e4c265a45654a2a5a5a854372c4843285
Thomas <sthx09567541@gmail.com>
2023-09-11 05:36:52 -0500

    migrate DB users-roles

--- Lab2_Crud/crud/models.py
+++ Lab2_Crud/crud/models.py
@@ -3,20 +3,21 @@ from django.utils import timezone
 
 # Create your models here.
 # Tables created here will be created in the database
+class Roles(models.Model):
+    rol_name = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    create_date = models.DateTimeField(default=timezone.now)
+    rol_status = models.BooleanField(default=True)
+
+# Define the Users model
 class Users(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     type_document = models.CharField(max_length=20)
     document = models.PositiveSmallIntegerField()
-    birthday = models.DateField() #Formato (DD-MM-AAA), {en caso de no funcionar = models.DateField(input_formats=["%d-%m-%Y"])}
+    birthday = models.DateField()
     phone_number = models.PositiveSmallIntegerField()
-    is_active = models.BooleanField(default=True) #El estado se deja como activo predeterminadamente
-    register_date = models.DateTimeField(default=timezone.now) #Establece la fecha actual como predeterminada
-    adress = models.CharField(max_length=30)
-    role = models.ForeignKey(Roles, on_delete=models.CASCADE, related_name='users') #ForeignKey para establecer la relación de Users con el modelo Roles
-
-class Roles(models.Model):
-    rol_name = models.CharField(max_length=30)
-    description = models.CharField(max_length=30)
-    create_date = models.DateTimeField(default=timezone.now) #Establece la fecha actual como predeterminada
-    rol_status  = models.BooleanField(default=True) #El estado se deja como activo predeterminadamente
+    is_active = models.BooleanField(default=True)
+    register_date = models.DateTimeField(default=timezone.now)
+    address = models.CharField(max_length=30)
+    role = models.ForeignKey(Roles, on_delete=models.CASCADE, related_name='users')
