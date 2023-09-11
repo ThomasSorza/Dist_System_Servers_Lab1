fece63d46cbaff408d86c87857c8283251510a73
Thomas <sthx09567541@gmail.com>
2023-09-10 22:08:09 -0500

    added basic view for lab #2

--- Lab2_Crud/crud/models.py
+++ Lab2_Crud/crud/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
 # Create your models here.
+# Tables created here will be created in the database
--- Lab2_Crud/crud/views.py
+++ Lab2_Crud/crud/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# TODO: add a view (Actually not allowed for lab #2)
+def hello(request):
+    return HttpResponse("<h1> Lab #2: A simple CRUD with HTTP. <br> Hello World!</h1>")
